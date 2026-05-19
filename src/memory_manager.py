d67c5036bdabffbdccf065a74640970013c1ea4e
shuaibiqbal <shuaibiqbaldgk@gmail.com>
2026-05-19 19:14:18 +0500

    memory_mangement code added

--- src/memory_manager.py
+++ src/memory_manager.py
@@ -10,7 +10,27 @@ def get_buffer_memory():
     return memory
     
 def get_window_memory(window_size):
-    memory = ConversationBufferWindowMemory(chat_memory="chat_history",
+    memory = ConversationBufferWindowMemory(memory_key="chat_history",
                                             return_messages=True,
                                             k=window_size) 
     return memory
+
+if __name__ == "__main__":
+    window_size = 2
+    if window_size is not None:
+        memory = get_window_memory(window_size)
+    else:
+        memory = get_buffer_memory()
+    memory.save_context(
+        {"input":"Hi"},
+        {"output": "Hello!"}
+    )
+    memory.save_context(
+        {"input": "my name is shuaib"},
+        {"output": "nice to meet you"}
+    )
+    memory.save_context(
+        {"input":"I love Ai"},
+        {"output": "AI is Amazing"}
+    )
+    print(memory.load_memory_variables({}))
